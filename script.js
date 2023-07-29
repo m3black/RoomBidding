e2e0c307f099aaeb16074ef783c8d9128ef2ad96
m3black <118626972+m3black@users.noreply.github.com>
2023-07-28 23:49:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,219 @@
-<script>
-    // Store the bid data for each bedroom
-    var bedroom1Bids = [];
-    var bedroom2Bids = [];
-    var bedroom3Bids = [];
-    var bedroom4Bids = [];
+// Function to fetch data from the GitHub Pages JSON file
+async function fetchBidsData() {
+    try {
+        const response = await fetch('/bids_data.json');
+        const bidsData = await response.json();
+        return bidsData;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
 
-    function submitBid(bedroom) {
-        var bidInput = document.getElementById(bedroom + "-bid");
-        var nameInput = document.getElementById(bedroom + "-name");
-        var bid = bidInput.value;
-        var name = nameInput.value;
+// Function to save data to the GitHub Pages JSON file
+async function saveBidsData(bidsData) {
+    try {
+        const response = await fetch('/bids_data.json', {
+            method: 'PUT', // Use the appropriate HTTP method here (PUT, POST, etc.)
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bidsData),
+        });
+        return response.ok;
+    } catch (error) {
+        console.error('Error saving data:', error);
+        return false;
+    }
+}
 
-        // Validate the bid input
-        if (bid === "" || isNaN(parseFloat(bid))) {
-            alert("Please enter a valid bid price.");
+// Initialize the bid data for each bedroom
+let bedroom1Bids, bedroom2Bids, bedroom3Bids, bedroom4Bids;
+
+// Run this function on page load to display the existing bids
+window.onload = async function () {
+    const bidsData = await fetchBidsData();
+    if (bidsData) {
+        bedroom1Bids = bidsData.bedroom1Bids;
+        bedroom2Bids = bidsData.bedroom2Bids;
+        bedroom3Bids = bidsData.bedroom3Bids;
+        bedroom4Bids = bidsData.bedroom4Bids;
+        updateTable("bedroom1", bedroom1Bids);
+        updateTable("bedroom2", bedroom2Bids);
+        updateTable("bedroom3", bedroom3Bids);
+        updateTable("bedroom4", bedroom4Bids);
+    } else {
+        bedroom1Bids = [];
+        bedroom2Bids = [];
+        bedroom3Bids = [];
+        bedroom4Bids = [];
+    }
+};
+
+function submitBid(bedroom) {
+    var bidInput = document.getElementById(bedroom + "-bid");
+    var nameInput = document.getElementById(bedroom + "-name");
+    var bid = bidInput.value;
+    var name = nameInput.value;
+
+    // Validate the bid input
+    if (bid === "" || isNaN(parseFloat(bid))) {
+        alert("Please enter a valid bid price.");
+        return;
+    }
+
+    // Validate the name input
+    if (name === "") {
+        alert("Please enter your name.");
+        return;
+    }
+
+    var currentBids;
+    var highestBid = 0;
+
+    // Get the current bid data for the respective bedroom
+    switch (bedroom) {
+        case "bedroom1":
+            currentBids = bedroom1Bids;
+            break;
+        case "bedroom2":
+            currentBids = bedroom2Bids;
+            break;
+        case "bedroom3":
+            currentBids = bedroom3Bids;
+            break;
+        case "bedroom4":
+            currentBids = bedroom4Bids;
+            break;
+        default:
             return;
-        }
+    }
 
-        // Update the bid data for the respective bedroom
+    // Find the highest bid in the current bid data
+    currentBids.forEach(function (bid) {
+        if (bid.bid > highestBid) {
+            highestBid = bid.bid;
+        }
+    });
+
+    // Check if the new bid is at least $10 higher than the highest bid
+    if (parseFloat(bid) <= highestBid + 10) {
+        alert("Please enter a bid that is at least $10 higher than the current highest bid.");
+        return;
+    }
+
+    // Update the bid data for the respective bedroom
+    currentBids.push({ name: name, bid: parseFloat(bid) });
+    updateTable(bedroom, currentBids);
+
+    // Clear the input fields after submission
+    bidInput.value = "";
+    nameInput.value = "";
+
+    // Save the bid data to GitHub Pages JSON file
+    saveBidsData({
+        bedroom1Bids,
+        bedroom2Bids,
+        bedroom3Bids,
+        bedroom4Bids,
+    });
+}
+
+function resetBids() {
+    // Clear the bid data for each bedroom
+    bedroom1Bids = [];
+    bedroom2Bids = [];
+    bedroom3Bids = [];
+    bedroom4Bids = [];
+
+    // Clear the tables
+    var tables = document.querySelectorAll("table");
+    tables.forEach(function (table) {
+        table.innerHTML = "";
+    });
+
+    // Save the updated bid data to GitHub Pages JSON file
+    saveBidsData({
+        bedroom1Bids,
+        bedroom2Bids,
+        bedroom3Bids,
+        bedroom4Bids,
+    });
+}
+
+function removeBid(bedroom, index) {
+    var password = prompt("Enter password to remove bid:");
+    if (password === "your_password") {
+        var currentBids;
+
+        // Get the current bid data for the respective bedroom
         switch (bedroom) {
             case "bedroom1":
-                bedroom1Bids.push({ name: name, bid: parseFloat(bid) });
-                updateChart("bedroom1", bedroom1Bids);
+                currentBids = bedroom1Bids;
                 break;
             case "bedroom2":
-                bedroom2Bids.push({ name: name, bid: parseFloat(bid) });
-                updateChart("bedroom2", bedroom2Bids);
+                currentBids = bedroom2Bids;
                 break;
             case "bedroom3":
-                bedroom3Bids.push({ name: name, bid: parseFloat(bid) });
-                updateChart("bedroom3", bedroom3Bids);
+                currentBids = bedroom3Bids;
                 break;
             case "bedroom4":
-                bedroom4Bids.push({ name: name, bid: parseFloat(bid) });
-                updateChart("bedroom4", bedroom4Bids);
+                currentBids = bedroom4Bids;
                 break;
             default:
                 return;
         }
 
-        // Clear the input fields after submission
-        bidInput.value = "";
-        nameInput.value = "";
+        // Remove the bid from the current bid data
+        currentBids.splice(index, 1);
+        updateTable(bedroom, currentBids);
+
+        // Save the updated bid data to GitHub Pages JSON file
+        saveBidsData({
+            bedroom1Bids,
+            bedroom2Bids,
+            bedroom3Bids,
+            bedroom4Bids,
+        });
+    } else {
+        alert("Incorrect password. Bid removal canceled.");
     }
+}
 
-    // Update the chart for a specific bedroom
-    function updateChart(bedroom, bids) {
-        var chartDiv = document.getElementById(bedroom + "-chart");
 
-        // Clear the chart before updating
-        chartDiv.innerHTML = "";
+// Your existing updateTable function remains the same.
+function updateTable(bedroom, bids) {
+    var table = document.getElementById(bedroom + "-table");
 
-        // Sort the bids in descending order based on bid price
-        bids.sort(function (a, b) {
-            return b.bid - a.bid;
+    // Clear the table body before updating
+    table.innerHTML = "";
+
+    // Sort the bids in descending order based on bid price
+    bids.sort(function (a, b) {
+        return b.bid - a.bid;
+    });
+
+    // Add each bid as a row in the table
+    bids.forEach(function (bid, index) {
+        var row = document.createElement("tr");
+        var nameCell = document.createElement("td");
+        var bidCell = document.createElement("td");
+        var removeCell = document.createElement("td");
+        var removeButton = document.createElement("button");
+
+        nameCell.textContent = bid.name;
+        bidCell.textContent = "$" + bid.bid.toFixed(2);
+
+        removeButton.textContent = "Remove";
+        removeButton.addEventListener("click", function () {
+            removeBid(bedroom, index);
         });
 
-        // Display the bids in the chart
-        bids.forEach(function (bid) {
-            var bidEntry = document.createElement("div");
-            bidEntry.textContent = bid.name + ": $" + bid.bid.toFixed(2);
-            chartDiv.appendChild(bidEntry);
-        });
+        removeCell.appendChild(removeButton);
 
-        // Determine the highest bid and display the winner
-        if (bids.length > 0) {
-            var highestBid = bids[0];
-            var winnerLabel = document.createElement("div");
-            winnerLabel.textContent = "Winner: " + highestBid.name + " ($" + highestBid.bid.toFixed(2) + ")";
-            chartDiv.appendChild(winnerLabel);
-        }
-    }
-</script>
+        row.appendChild(nameCell);
+        row.appendChild(bidCell);
+        row.appendChild(removeCell);
+        table.appendChild(row);
+    });
+}
